64e0deb26a784d316151135a19d4b8592b2c0cae
Drew Miller <drew@onpatrol.co>
2014-01-11 21:09:36 +0000

    Moved routes file to resources folder to suit express-resource better

---
+++ resources/articles.js
@@ -0,0 +1,53 @@
+var db = require('../models');
+
+// GET /articles
+exports.index = function(req, res) {
+  db.Article.findAll().success(function(articles) {
+    res.render('articles/index', { articles: articles });
+  });
+};
+
+// GET /articles/new
+exports.new = function(req, res) {
+  res.render('articles/new');
+};
+
+// POST /articles
+exports.create = function(req, res) {
+  db.Article.create({ title: req.body.article_title, text: req.body.article_text })
+    .success(function(article) {
+      res.redirect('/articles/' + article.getDataValue('id'));
+    })
+    .error(function(error) {
+      console.log('something fucked up creating: ', error);
+      res.send('something fucked up: ' + error);
+    });
+};
+
+// GET /articles/:id
+exports.show = function(req, res) {
+  db.Article.find({ where: { id: req.params.article } })
+    .success(function(article) {
+      res.render('articles/show', { article: article });
+    })
+    .error(function(error) {
+      console.log('something fucked up showing: ', error);
+      res.send('something fucked up: ' + error);
+    });
+};
+
+// GET /articles/:id/edit
+exports.edit = function(req, res) {
+  res.send('EDIT WOO!');
+};
+
+// PUT /articles/:id
+exports.update = function(req, res) {
+  res.send('UPDATE WOO!');
+};
+
+// DELETE /articles/:id
+exports.destroy= function(req, res) {
+  res.send('DELETE WOO!');
+};
+
