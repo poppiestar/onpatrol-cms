989d936229c97b3d53312653a5147cd66d4c337b
Drew Miller <drew@onpatrol.co>
2014-01-25 13:07:24 +0000

    Updated model to set visible default value to be true

--- models/category_model.js
+++ models/category_model.js
@@ -1,7 +1,11 @@
 module.exports = function(sequelize, DataTypes) {
   var Category = sequelize.define('Category', {
     name: DataTypes.STRING,
-    visible: DataTypes.BOOLEAN
+    visible: {
+      type: DataTypes.BOOLEAN,
+      allowNull: false,
+      defaultValue: true
+    }
   });
 
   return Category;
